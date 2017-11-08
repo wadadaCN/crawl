b8633c2da240521a0de80f6b50778e8cf79ca000
wadadaCN <xie_kun@163.com>
2017-11-08 11:48:01 +0800

    增加了从redis读出数据写入csv文件

--- zhihu/nospl_redis.py
+++ zhihu/nospl_redis.py
@@ -1,5 +1,5 @@
 # code = python3
-# encoding = utf-8
+# encoding = bytes
 # wrote by Xie on 2017/11/7
 # connected to the redis
 
@@ -13,8 +13,27 @@ def get_redis():
     except:
         print('redis数据库连接失败！')
 
+def redis2csv(startStr,nameStr):
+    r = get_redis()
+    with open('{}.csv'.format(nameStr), 'wb') as file:
+        file.write(bytes(startStr,'utf-8'))
+        while 1:
+
+            item = r.spop('{}'.format(nameStr))
+            if item != None:
+                try: #某些字符写入不支持
+                    file.write(item)
+                except:
+                    continue
+            else:
+                break
+
+    print("写入完成")
+
 
 if __name__ == '__main__':
-    r = get_redis()
-    r.sadd('test', '1')
-    r.sadd('test', '2')
+    # r = get_redis()
+    # r.sadd('test', '1s')
+    # r.sadd('test', '2s')
+    startStr = "start\n"
+    redis2csv(startStr, 'info_list')
