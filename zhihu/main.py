8b6f69f1d8db5209f9f0782fad3bc8333be59fc7
wadadaCN <xie_kun@163.com>
2017-11-18 21:22:34 +0800

    修改了数据结构

--- zhihu/main.py
+++ zhihu/main.py
@@ -6,18 +6,20 @@
 import requests
 import re
 from time import sleep
+from multiprocessing import Pool
 
-from getProxies import redisCLI,ipTest,get66ip
+from getProxies import redisCLI,ipTest
 from nosql_redis import redis2csv
 
 headers = {
-'authorization':'Bearer Mi4xcEpzcUFBQUFBQUFBRUVKVmJTSkhEQmNBQUFCaEFsVk5pUEVMV2dEcUFKV0xaQUp5QW9Talg5WlRtbV9nM1NTSk1R|1508140168|a5cc04d035818b42b088adfa011750eca648bcdc',
+'authorization':'Bearer Mi4xcEpzcUFBQUFBQUFBRUVKVmJTSkhEQmNBQUFCaEFsVk5JM185V2dEYjF1U1dPLWN1am5xc2xqREtmMl9QdVN2dWF3|1511010595|10adf8a6d505ba0a0b0d15607e9015dfb7fbf3bf',
 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36',
 }
 
 def main(userToken,proxy,r):
     url = "https://www.zhihu.com/api/v4/members/{}/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=0&limit=20".format(userToken)
     offset = 0
+    url_tokenList = []
     while 1:
         response = ""
         try:
@@ -37,11 +39,13 @@ def main(userToken,proxy,r):
             name = userItem['name']
             follower_count = userItem['follower_count']
             url_token = userItem['url_token']
+            url_tokenList.append(url_token)
             id = userItem['id']
             info = "{0},{1},{2},{3},{4},{5},{6}\n".format(name,headline,answer_count,articles_count,follower_count,url_token,id)
-            if not r.sismember('visited',url_token):
+            if not r.hexists('visited',url_token):
                 r.sadd('info_list',info)
                 r.sadd('user_list',url_token)
+        r.hset('visited',userToken,url_tokenList)
 
         if offset < totalFollower:
             offset += 20
@@ -49,21 +53,36 @@ def main(userToken,proxy,r):
         else:
             break
 
-
-if __name__ == '__main__':
-    # get66ip()
-    startUserToken = 'zhang-jia-wei'
-    info = "用户名,简介,回答数,文章数,粉丝数,用户唯一认证,id\n"
-    proxy = ipTest(redisCLI)
-    main(startUserToken, proxy, redisCLI)
+def start():
+    try:
+        proxy = ipTest(redisCLI)
+    except ConnectionError:
+        print('所有代理站点不可用！')
     while 1:
         sleep(3)
         try:
             userToken = redisCLI.spop('user_list')
-            if not redisCLI.sismember('visited',userToken):
-                redisCLI.sadd('visited',userToken)
+            if not redisCLI.hexists('visited',userToken):
+                redisCLI.hset('visited',userToken,None)
                 userToken = userToken.decode() #将bytes对象转换为str对象
                 main(userToken, proxy, redisCLI)
         except:
             break
-    redis2csv(info,'visited')
+    raise InterruptedError
+
+if __name__ == '__main__':
+    while 1:
+        # p = Pool(4)
+        # # startUserToken = 'zhang-jia-wei'
+        # # info = "用户名,简介,回答数,文章数,粉丝数,用户唯一认证,id\n"
+        # # main(startUserToken, proxy, redisCLI)
+        # # redis2csv(info,'visited')
+        try:
+            # p.apply_async(start)
+            start()
+        except InterruptedError:
+            break
+        else:
+            print('error')
+        # p.join()
+    print('done')
